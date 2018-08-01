07216b9e3c142f35e9da9568538bc12ed233d581
Ubuntu <ubuntu@ip-172-31-38-216.us-east-2.compute.internal>
2018-08-01 17:46:28 +0000

    Added deletion and solving

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from flask import render_template
 from CTFd.utils import admins_only, is_admin
-from CTFd.models import db, Challenges, Keys, Awards, Solves, Files, Tags, Teams
+from CTFd.models import db, Challenges, Keys, Awards, Solves, Files, Tags, Teams, WrongKeys
 from CTFd import utils
 from logging import getLogger, basicConfig,DEBUG,ERROR
 from CTFd.plugins import challenges, register_plugin_assets_directory
 from CTFd import utils, CTFdFlask
 from flask import session
-
+from CTFd.plugins.keys import get_key_class
 
 
 
@@ -53,6 +53,8 @@ class SmartCity(challenges.BaseChallenge):
 		:param request:
 		:return:
 		"""
+		
+		files = request.files.getlist('files[]')
 	
 		chal = SmartCityChallenge(
 			name= request.form['name'],
@@ -69,15 +71,28 @@ class SmartCity(challenges.BaseChallenge):
 		else:
 			chal.hidden = False
 
-		files = request.files.getlist('files[]')
-		for f in files:
-			utils.upload_file(file=f, chalid=chal.id)
+		max_attempts = request.form.get('max_attempts')
+		if max_attempts and max_attempts.isdigit():
+			chal.max_attempts = int(max_attempts)
+
 
 
 		logger.debug("Genereted buildingId " + chal.buildingId + " for challenge " + chal.name)
 		
 		db.session.add(chal)
 		db.session.commit() 
+
+		flag = Keys(chal.id, request.form['key'], request.form['key_type[0]'])
+		if request.form.get('keydata'):
+			flag.data = request.form.get('keydata')
+		
+		db.session.add(flag)
+		db.session.commit()
+
+		for f in files:
+			utils.upload_file(file=f, chalid=chal.id)
+
+		db.session.commit()
 
 	
 	@staticmethod
@@ -103,6 +118,94 @@ class SmartCity(challenges.BaseChallenge):
 		
 		}
 		return challenge, data
+
+	@staticmethod
+	def delete(challenge):
+		"""
+        	This method is used to delete the resources used by a challenge.
+
+        	:param challenge:
+        	:return:
+       		 """
+        	WrongKeys.query.filter_by(chalid=challenge.id).delete()
+        	Solves.query.filter_by(chalid=challenge.id).delete()
+        	Keys.query.filter_by(chal=challenge.id).delete()
+        	files = Files.query.filter_by(chal=challenge.id).all()
+        	for f in files:
+            		utils.delete_file(f.id)
+        	Files.query.filter_by(chal=challenge.id).delete()
+        	Tags.query.filter_by(chal=challenge.id).delete()
+        	SmartCityChallenge.query.filter_by(id=challenge.id).delete()
+        	Challenges.query.filter_by(id=challenge.id).delete()
+        	db.session.commit()
+
+
+	@staticmethod
+    	def attempt(chal, request):
+        	"""
+        	This method is used to check whether a given input is right or wrong. It does not make any changes and should
+        	return a boolean for correctness and a string to be shown to the user. It is also in charge of parsing the
+        	user's input from the request itself.
+
+        	:param chal: The Challenge object from the database
+        	:param request: The request the user submitted
+        	:return: (boolean, string)
+        	"""
+		print("hello world")
+        	provided_key = request.form['key'].strip()
+        	chal_keys = Keys.query.filter_by(chal=chal.id).all()
+		print(chal_keys) 
+        	for chal_key in chal_keys:
+			print(chal_key)
+            		if get_key_class(chal_key.type).compare(chal_key, provided_key):
+                		return True, 'Correct'
+        	return False, 'Incorrect'
+
+    	@staticmethod
+    	def solve(team, chal, request):
+        	"""
+        	This method is used to insert Solves into the database in order to mark a challenge as solved.
+
+        	:param team: The Team object from the database
+        	:param chal: The Challenge object from the database
+        	:param request: The request the user submitted
+        	:return:
+        	"""
+        	chal = SmartCityChallenge.query.filter_by(id=chal.id).first()
+
+        	solve_count = Solves.query.join(Teams, Solves.teamid == Teams.id).filter(Solves.chalid==chal.id, Teams.banned==False).count()
+
+        	#value = (((chal.minimum - chal.initial)/(chal.decay**2)) * (solve_count**2)) + chal.initial
+        	#value = math.ceil(value)
+
+        	#if value < chal.minimum:
+            	#	value = chal.minimum
+
+        	#chal.value = value
+
+        	provided_key = request.form['key'].strip()
+        	solve = Solves(teamid=team.id, chalid=chal.id, ip=utils.get_ip(req=request), flag=provided_key)
+        	db.session.add(solve)
+
+        	db.session.commit()
+        	db.session.close()
+
+    	@staticmethod
+    	def fail(team, chal, request):
+        	"""
+        	This method is used to insert WrongKeys into the database in order to mark an answer incorrect.
+
+        	:param team: The Team object from the database
+        	:param chal: The Challenge object from the database
+        	:param request: The request the user submitted
+        	:return:
+        	"""
+        	provided_key = request.form['key'].strip()
+        	wrong = WrongKeys(teamid=team.id, chalid=chal.id, ip=utils.get_ip(request), flag=provided_key)
+        	db.session.add(wrong)
+        	db.session.commit()
+        	db.session.close()
+
 
 def load(app):
     """load overrides for smart_city to work properly"""
