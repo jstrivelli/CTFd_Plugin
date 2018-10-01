d73f48c574538abbf83574d78058d28935868ac6
Ubuntu <ubuntu@ip-172-31-38-216.us-east-2.compute.internal>
2018-10-01 16:48:37 +0000

    trying something here

--- __init__.py
+++ __init__.py
@@ -677,11 +677,7 @@ def setup_custom():
     <div class="col-md-6 offset-md-3">
         
         <h3 class="text-center">
-            <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
-            <p>Follow us on social media:</p>
-            <a href="https://twitter.com/ctfdio"><i class="fab fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://facebook.com/ctfdio"><i class="fab fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;
-            <a href="https://github.com/ctfd"><i class="fab fa-github fa-2x" aria-hidden="true"></i></a>
+	    <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="https://i.gifer.com/758h.gif" />
         </h3>
         <br>
         <h4 class="text-center">
